b33410577a58bf52e32590bcbad7d709ed562b84
Geoglyph000 <nasca19970911@gmail.com>
2021-09-09 08:13:44 +0000

    edit sign_in form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_181514) do
+ActiveRecord::Schema.define(version: 2021_09_09_075744) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 2021_09_06_181514) do
     t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
+    t.string "profile_image_id"
+    t.boolean "is_active", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
