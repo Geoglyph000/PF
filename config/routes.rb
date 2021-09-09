88f5613d5f861e6fbef8bae9bf77d1b5dc932550
Geoglyph000 <nasca19970911@gmail.com>
2021-09-09 02:32:46 +0000

    initial routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   namespace :admin do
+    resources :admins, only: [:show, :edit, :update]
+    resources :users, except: [:destroy]
+    resources :posts
+    resources :genres
   end
+
+  #エンドユーザ
+  resources :users, only: [:show, :edit, :update]
+  resources :posts
 
 end
