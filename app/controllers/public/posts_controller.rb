56a5763ce3c0e73b67496d8ee470656fe931bc0b
Geoglyph000 <nasca19970911@gmail.com>
2021-09-09 05:17:36 +0000

    install post function, index showpage

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,34 @@
 class Public::PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to posts_path
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :date)
+  end
+
 end
