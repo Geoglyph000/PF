0bc1387f1b144b1eb0c0c2b2f6e2a51a48101456
Geoglyph000 <nasca19970911@gmail.com>
2021-09-11 05:59:08 +0000

    add refile select key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '534a0d8dd1961450ef8f7639543e7a932d9daae08bad714a66093c3918198d51a632c24aff77d8dce54dba2e1dee694fef2501adff0962c22a31aa4291755376'
