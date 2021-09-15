f4fc71c682ed7d287247c339987643328b9b8712
Geoglyph000 <nasca19970911@gmail.com>
2021-09-15 06:01:14 +0000

    install user-logical-delete

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -15,6 +15,13 @@ class Public::UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
+  def deactivate
+    @user = User.find(params[:id])
+    @user.update(is_active: false) #退会フラグを立てる
+    reset_session #ログアウトさせる
+    redirect_to root_path #ホームにリダイレクト
+  end
+
   private
   def user_params
     params.require(:user).permit(:user_name, :introduction, :profile_image)
