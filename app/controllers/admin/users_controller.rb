3d75eee213475177e6e44b9c5b8bfa5f84287b3a
Geoglyph000 <nasca19970911@gmail.com>
2021-09-22 06:34:44 +0000

    add whole post deactive function to admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,26 +17,21 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      if @user.is_active == false #ユーザを論理削除した際に
+        @user.posts.all.update(is_private: true) #ユーザの投稿を全て非公開に
+      end
       redirect_to admin_user_path(@user.id)
     else
       render :edit
     end
   end
 
-  def destroy #物理削除
+  def destroy #ユーザの物理削除
     user = User.find(params[:id])
     user.destroy
     redirect_to root_path
   end
 
-  def deactivate #論理削除
-    @user = User.find(params[:id])
-    @posts = @user.posts.all
-    @user.update(is_active: false) #退会フラグを立てる
-    @posts.update(is_private: true) #投稿を全て非公開にする
-    reset_session #ログアウトさせる
-    redirect_to root_path #ホームにリダイレクト
-  end
 
   private
     def user_params
