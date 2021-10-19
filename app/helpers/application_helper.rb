8f568a3d621c962591a124497a040afa3532c1cb
Geoglyph000 <nasca19970911@gmail.com>
2021-10-19 06:43:39 +0000

    set titles for each viewpages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #ページごとにタイトルを変更する
+  def full_title(page_title = '')
+    base_title = "Shareversary"
+      if page_title.empty? #タイトルが設定されていない場合
+        base_title #サイト名だけ表示
+      else
+        "#{ page_title } / #{ base_title }"
+      end
+  end
 end
